7bc0c7c5662e3c98b71daf83c44f280f1d4a8901
Komal-5 <68140176+Komal-5@users.noreply.github.com>
2022-04-26 20:27:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 var randomNumber1=Math.random();
 randomNumber1=randomNumber1*6;
 randomNumber1=Math.floor(randomNumber1)+1;
-document.querySelector(".dice .img1").setAttribute("src","images/dice" + randomNumber1 + ".png");
+document.querySelector(".dice .img1").setAttribute("src","dice" + randomNumber1 + ".png");
 // document.querySelectorAll("img")[0].setAttribute("src","images/dice" + randomNumber1 + ".png");
 var randomNumber2=Math.random();
 randomNumber2=randomNumber2*6;
 randomNumber2=Math.floor(randomNumber2)+1;
-document.querySelector(".dice .img2").setAttribute("src","images/dice" + randomNumber2 + ".png");
+document.querySelector(".dice .img2").setAttribute("src","dice" + randomNumber2 + ".png");
 
 if(randomNumber1>randomNumber2){
   document.querySelector("h1").innerHTML= "🚩 Player 1 Wins!";
